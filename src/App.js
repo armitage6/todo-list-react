1ad46ca78a3318b1eae962d9647798305a9e48b7
Armitage <pablutus@gmail.com>
2023-02-13 19:49:28 -0300

    subiendo todo-list-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [todos, setTodos] = useState([])
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Todo List</h1>
+    <div className='containerr'>
+      <ul>
+        <li>
+          <input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyUp={(e) => {
+              if (e.key === "Enter" && inputValue !== "") {
+                setTodos(todos.concat(inputValue));
+                setInputValue('');
+              }
+            }}
+            type='text'
+            placeholder='What do you need to do'
+          />
+        </li>
+        {todos.length === 0 ? (
+          <li>No hay tareas.</li>
+        ) : (
+          todos.map((item, index) => (
+            <li key={index}>
+              {item}
+              {' '}
+              <i
+                className='fa-solid fa-xmark'
+                onClick={() =>
+                  setTodos(
+                    todos.filter(
+                      (t, currentIndex) => index !== currentIndex
+                    )
+                  )
+                }
+              ></i>
+            </li>
+          ))
+        )}
+      </ul>
+      <div>{todos.length} tasks</div>
+     
     </div>
-  );
+
+
+    {todos.length > 0 && (
+        <button
+        className='mt-2 btn btn-light'
+          onClick={() => {
+            setTodos([]);
+          }}
+        >
+          Eliminar todas las tareas
+        </button>
+      )}
+  </>
+  )
+    
+    
+  
 }
 
 export default App;
